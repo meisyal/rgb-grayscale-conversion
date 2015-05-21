25275faeca753f0ee699512ab760a49605a444f9
Andrias Meisyal <andriasonline@gmail.com>
2015-05-22 01:31:23 +0700

    receive image files

--- src/main/java/com/sister/kelompok5/Message.java
+++ src/main/java/com/sister/kelompok5/Message.java
@@ -4,8 +4,14 @@
  */
 package com.sister.kelompok5;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+
+import javax.imageio.ImageIO;
 
 public class Message extends UnicastRemoteObject implements MessageInterface {
 
@@ -16,9 +22,15 @@ public class Message extends UnicastRemoteObject implements MessageInterface {
 
 	public Message() throws RemoteException {        
     }
-    
-    public void Echo(String name) throws RemoteException {
-        System.out.println("Echo from client: " + name);
-    }
+
+	public void Convert(byte[] imageByteArray, int fileNumber, String fileName, String fileExtension) throws IOException {
+		BufferedImage image = ImageIO.read(new ByteArrayInputStream(imageByteArray));
+				
+		String pathNewFile = "";
+			
+		File path = new File(pathNewFile + fileNumber + "_" + fileName + "_grayscale." + fileExtension);
+			
+		ImageIO.write(image, fileExtension, path);
+	}
     
 }
